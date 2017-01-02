719f92f6c7a77203cea1068f7689f9aff998d773
Tongcheng Li <tl486@cornell.edu>
2017-01-02 20:28:07 +0800

    change hpp

--- include/caffe/layers/DenseBlock_layer.hpp
+++ include/caffe/layers/DenseBlock_layer.hpp
@@ -12,11 +12,13 @@
 namespace caffe {
 
 template <typename Dtype>
-class DenseBlockLayer : public NeuronLayer<Dtype> {
+class DenseBlockLayer : public Layer<Dtype> {
  public:
   explicit DenseBlockLayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
+      : Layer<Dtype>(param) {}
 
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top); 
   virtual inline const char* type() const { return "DenseBlock"; } 
 
  protected:
@@ -30,6 +32,20 @@ class DenseBlockLayer : public NeuronLayer<Dtype> {
   virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
+  //common Blobs for both CPU & GPU mode
+  Blob<Dtype> BN_Scaler, BN_Bias;
+  vector<Blob<Dtype>*> filter_Vec;
+  
+  //GPU ptr for efficient space usage only, these pointers not allocated when CPU_ONLY, this are not Blob because Descriptor is not traditional 
+  float* postConv_data_gpu;
+  float* postConv_grad_gpu;
+  float* postBN_data_gpu;
+  float* postBN_grad_gpu;
+  float* postReLU_data_gpu;
+  float* postReLU_grad_gpu;
+  
+
+  
 };
 
 }  // namespace caffe
