50eb805c6a82ff7c26dcfeb84b32658dbbf57f4a
Tongcheng Li <tl486@cornell.edu>
2017-01-03 23:25:49 +0800

    Parameterize DenseNet, TODO: descriptors

--- include/caffe/layers/DenseBlock_layer.hpp
+++ include/caffe/layers/DenseBlock_layer.hpp
@@ -7,8 +7,6 @@
 #include "caffe/layer.hpp"
 #include "caffe/proto/caffe.pb.h"
 
-#include "caffe/layers/neuron_layer.hpp"
-
 namespace caffe {
 
 template <typename Dtype>
@@ -18,34 +16,45 @@ class DenseBlockLayer : public Layer<Dtype> {
       : Layer<Dtype>(param) {}
 
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+  
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top); 
+  
   virtual inline const char* type() const { return "DenseBlock"; } 
 
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
+  
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
 
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  
   virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
   //common Blobs for both CPU & GPU mode
-  Blob<Dtype> BN_Scaler, BN_Bias;
-  vector<Blob<Dtype>*> filter_Vec;
+  //Blob<Dtype> BN_Scaler, BN_Bias;
+  //vector<Blob<Dtype>*> filter_Vec;
   
   //GPU ptr for efficient space usage only, these pointers not allocated when CPU_ONLY, this are not Blob because Descriptor is not traditional 
   float* postConv_data_gpu;
   float* postConv_grad_gpu;
   float* postBN_data_gpu;
-  float* postBN_grad_gpu;
+  float* postBN_grad_gpu;b
   float* postReLU_data_gpu;
   float* postReLU_grad_gpu;
+  float* workspace;
   
-
-  
+  int initChannel, growthRate, numTransition; 
+  int N,H,W; //N,H,W of the input tensor, inited in reshape phase
+  int trainCycleIdx; //used in BN train phase for EMA Mean/Var estimation
+  //convolution Related
+  int pad_h, pad_w, conv_verticalStride, conv_horizentalStride; 
+  int filter_H, filter_W;
+  //gpu workspace size
+  int workspace_size_bytes; 
 };
 
 }  // namespace caffe
--- src/caffe/layers/DenseBlock_layer.cpp
+++ src/caffe/layers/DenseBlock_layer.cpp
@@ -8,6 +8,70 @@
 namespace caffe {
 
   template <typename Dtype>
+  void DenseBlockLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top){
+	DenseBlockParameter dbParam = this->layer_param_.denseblock_param();
+        this->numTransition = dbParam.numTransition();
+        this->initChannel = dbParam.initChannel();
+        this->growthRate = dbParam.growthRate();
+        this->trainCycleIdx = 0; //initially, trainCycleIdx = 0
+        this->pad_h = dbParam.pad_h();
+        this->pad_w = dbParam.pad_w();
+        this->conv_verticalStride = dbParam.conv_verticalStride();
+        this->conv_horizentalStride = dbParam.conv_horizentalStride();
+        this->filter_H = dbParam.filter_H();
+        this->filter_W = dbParam.filter_W();
+        this->workspace_size_bytes = 10000000;
+        //Parameter Blobs
+        //blobs_[0] is BN scaler weights, blobs_[1] is BN bias weights
+        //blobs_[2:2+numTransition] is filter weights for convolution of each transition
+        this->blobs_.resize(2+this->numTransition);
+	int numChannelsTotal = this->initChannel + this->growthRate * this->numTransition;
+	vector<int> channelShape = {1,numChannelsTotal,1,1};
+	this->blobs_[0].reset(new Blob<Dtype>(channelShape));
+	this->blobs_[1].reset(new Blob<Dtype>(channelShape));
+	shared_ptr<Filler<Dtype>> weight_filler0(GetFiller<Dtype>(dbParam.BN_Scaler_Filler()));
+        weight_filler0->Fill(this->blobs_[0].get());
+        shared_ptr<Filler<Dtype>> weight_filler1(GetFiller<Dtype>(dbParam.BN_Bias_Filler()));
+        weight_filler1->Fill(this->blobs_[1].get());
+        for (int transitionIdx=0;transitionIdx < this->numTransition;++transitionIdx){
+	    int inChannels = initChannel + transitionIdx * growthRate;
+	    vector<int> filterShape = {growthRate,inChannels,filter_H,filter_W};
+	    this->blobs_[2+transitionIdx].reset(new Blob<Dtype>(filterShape));
+	    shared_ptr<Filler<Dtype>> filter_Filler(GetFiller<Dtype>(dbParam.Filter_Filler()));
+	    filter_Filler->Fill(this->blobs_[2+transitionIdx].get());
+	}
+	
+	
+}
+
+  template <typename Dtype>
+  void DenseBlockLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top){ 
+        this->N = bottom[0]->shape[0]; 
+        this->H = bottom[0]->shape[2];
+        this->W = bottom[0]->shape[3];
+//GPU intermediate ptrs
+#ifndef CPU_ONLY
+        int bufferSize_byte = this->N*(this->initChannel+this->growthRate*this->numTransition)*this->H*this->W*sizeof(Dtype);
+        cudaMalloc(&this->postConv_data_gpu,bufferSize_byte);
+        cudaMalloc(&this->postBN_data_gpu,bufferSize_byte);
+        cudaMalloc(&this->postReLU_data_gpu,bufferSize_byte);
+        cudaMalloc(&this->postConv_grad_gpu,bufferSize_byte);
+        cudaMalloc(&this->postBN_grad_gpu,bufferSize_byte);
+        cudaMalloc(&this->postReLU_grad_gpu,bufferSize_byte);
+
+        cudaMemset(this->postConv_data_gpu,0,bufferSize_byte);
+	cudaMemset(this->postBN_data_gpu,0,bufferSize_byte);
+	cudaMemset(this->postReLU_data_gpu,0,bufferSize_byte);
+	cudaMemset(this->postConv_grad_gpu,0,bufferSize_byte);
+	cudaMemset(this->postBN_grad_gpu,0,bufferSize_byte);
+	cudaMemset(this->postReLU_grad_gpu,0,bufferSize_byte);
+	//workspace
+	cudaMalloc(&this->workspace,this->workspace_size_bytes);
+	cudaMemset(this->workspace,0,this->workspace_size_bytes);
+#endif 
+  }
+
+  template <typename Dtype>
   void DenseBlockLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
                                   const vector<Blob<Dtype>*>& top) 
   { 
@@ -18,7 +82,6 @@ namespace caffe {
     for (int i = 0; i < count; ++i) {
       top_data[i] = sin(bottom_data[i]);
     }
-    //NOT_IMPLEMENTED;
   }
 
   template <typename Dtype>
@@ -37,7 +100,6 @@ namespace caffe {
         bottom_diff[i] = top_diff[i] * cos(bottom_datum);
       }
     }
-    //NOT_IMPLEMENTED;
   }
 
 #ifdef CPU_ONLY
