c81431c45f5ab1f11c8dac55bcc4d427606c2697
Tongcheng Li <tl486@cornell.edu>
2017-01-12 15:07:41 +0800

    change random seed

--- computeDiff.py
+++ computeDiff.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-fileNameA = "TClog/cpu_1/postReLU_blobVec_0_grad"
-fileNameB = "TClog/gpu_2/postReLU_grad_gpu"
+fileNameA = "TClog/cpu_3/postConv_blobVec_1_grad"
+fileNameB = "TClog/gpu_4/postConv_grad_gpu"
 A_offset = 0
-B_offset = 0
-rangeLen = 75
+B_offset = 75
+rangeLen = 50
 
 listA =  open(fileNameA,'r').readlines()[0].split(",")[:-1]
 listB =  open(fileNameB,'r').readlines()[0].split(',')[:-1]
@@ -14,13 +14,13 @@ floatBL = map(lambda x:float(x),listB)
 print len(floatAL)
 print floatAL[A_offset:A_offset+rangeLen]
 print floatBL[B_offset:B_offset+rangeLen]
-"""
+
 for i in range(rangeLen):
     numA,numB = floatAL[A_offset+i],floatBL[B_offset+i]
-    print numA-numB
     if abs(numA-numB)>0.2:
+        print `numA`+":"+`numB`+":"+`numA-numB`
         print i
-"""
+
 globalMaxDiff = 0 
 for i in range(rangeLen):
     aIdx,bIdx = A_offset+i,B_offset+i 
