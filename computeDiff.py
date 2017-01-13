78ff2d27f9801db44557fd7b9f8ae1e3b99130ef
Tongcheng Li <tl486@cornell.edu>
2017-01-13 17:23:42 +0800

    try

--- computeDiff.py
+++ computeDiff.py
@@ -1,15 +1,16 @@
 import numpy as np
 
-fileNameA = "TClog/cpu_9/bias_0_grad"
-fileNameB = "TClog/gpu_10/Bias_grad_gpu_0"
+fileNameA = "TClog/cpu_11/postConv_blobVec_0_grad"
+fileNameB = "TClog/gpu_12/postConv_grad_gpu"
 A_offset = 0
 B_offset = 0
-rangeLen = 3
+rangeLen = 75
 
 listA =  open(fileNameA,'r').readlines()[0].split(",")[:-1]
 listB =  open(fileNameB,'r').readlines()[0].split(',')[:-1]
 floatAL = map(lambda x:float(x),listA)
 floatBL = map(lambda x:float(x),listB)
+
 
 print len(floatAL)
 print floatAL[A_offset:A_offset+rangeLen]
@@ -17,7 +18,7 @@ print floatBL[B_offset:B_offset+rangeLen]
 
 for i in range(rangeLen):
     numA,numB = floatAL[A_offset+i],floatBL[B_offset+i]
-    if abs(numA-numB)>0.2:
+    if abs(numA-numB)>6:
         print `numA`+":"+`numB`+":"+`numA-numB`
         print i
 
@@ -27,4 +28,5 @@ for i in range(rangeLen):
     globalMaxDiff = max(globalMaxDiff,abs(floatAL[aIdx]-floatBL[bIdx]))
 
 print "global Max Diff is:"+`globalMaxDiff`
+
 
