c57e6b1ce1a9d87d1d5a2c7927ee0bbe80e6b344
Tongcheng Li <tl486@cornell.edu>
2017-01-26 16:48:11 -0500

    fix logId issue

--- src/caffe/test/test_DenseBlock_layer.cpp
+++ src/caffe/test/test_DenseBlock_layer.cpp
@@ -468,6 +468,9 @@ TYPED_TEST(DenseBlockLayerTest, TestTrueFwd){
   typedef typename TypeParam::Dtype Dtype;
   DenseBlockParameter* db_param = this->layer_param.mutable_denseblock_param();
   DenseBlockLayer<Dtype>* dbLayer = new DenseBlockLayer<Dtype>(this->layer_param);
+  global_id += 100;
+  dbLayer->setLodId(global_id);
+  global_id += 1;
   
   shared_ptr<Filler<Dtype> > gaussianFiller(GetFiller<Dtype>(db_param->filter_filler()));
   gaussianFiller->Fill(this->blob_bottom_cpu);
